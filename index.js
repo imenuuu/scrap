07dfaf2b941a91a3837e05c93188bade8db3ea7f
mdwgti26 <mdwgti16@epopcon.com>
2023-02-06 16:50:41 +0900

    - [detail post] error 예외 처리 추가 - [detail post] urls priority 추가

--- index.js
+++ index.js
@@ -8,14 +8,22 @@ const bodyParser = require('body-parser');
 const chromeConfig = require('./src/config/chrome/ChromeConfig').options;
 const service = require('./src/config/service.json');
 const validator = require('./src/util/ValidatorUtil');
+const {del} = require("express/lib/application");
 
 const urls = {};
-const queueLength = 10;
+const priorities = {};
+const queueLength = 1;
 
 function sleep(ms) {
     return new Promise((resolve) => {
         setTimeout(resolve, ms);
     });
+}
+
+function getPrimaryPriorityKey() {
+    return Object.keys(priorities).sort((k1, k2) => {
+        return priorities[k1] - priorities[k2];
+    })[0];
 }
 
 async function launch() {
@@ -28,29 +36,45 @@ async function launch() {
     // logger.info(`item: ${item}`)
 
     app.post('/detail', async (req, res) => {
-        while (Object.keys(urls).length >= queueLength) {
-            await sleep(1000);
-            console.log(`queue is full, length: ${Object.keys(urls).length}`);
+        try {
+            const collectSite = req.body.collectSite;
+            const url = req.body.url;
+            let key = `${collectSite}==${url}`;
+            priorities[key] = new Date();
+
+            while (true) {
+                if (Object.keys(urls).length < queueLength && key === getPrimaryPriorityKey()) {
+                    break;
+                }
+
+                await sleep(1000);
+                console.log(`queue is full, length: ${Object.keys(urls).length}, ${Object.keys(priorities).length}`);
+            }
+            delete priorities[key];
+            urls[key] = key;
+            logger.info(`${key}, ${Object.keys(urls).length}, ${Object.keys(priorities).length}`);
+
+
+            let item = null;
+            try {
+                let CLASS_PATH = validator.validateClassPath(service.detail, collectSite);
+                let task = new detailTask(collectSite, CLASS_PATH, chromeConfig);
+                item = await task.execute(url);
+            } catch (e) {
+                logger.error("detailTask error", e);
+            }
+
+            logger.info(`item: ${item}`);
+            // res.send(`CollectSite ${collectSite}, url is ${url}, item is ${item}`)
+            res.send({
+                result: `CollectSite ${collectSite}, url is ${url}, item is ${item}`,
+                item: JSON.parse(JSON.stringify(item))
+            });
+
+            delete urls[key];
+        } catch (e){
+            logger.error('post error', e)
         }
-        const collectSite = req.body.collectSite;
-        const url = req.body.url;
-        let key = `${collectSite}==${url}`;
-        urls[key] = key;
-        console.log(key, Object.keys(urls).length);
-
-        let CLASS_PATH = validator.validateClassPath(service.detail, collectSite);
-        let task = new detailTask(collectSite, CLASS_PATH, chromeConfig);
-        let item = await task.execute(url);
-        logger.info(`item: ${item}`);
-
-
-        // res.send(`CollectSite ${collectSite}, url is ${url}, item is ${item}`)
-        res.send({
-            result: `CollectSite ${collectSite}, url is ${url}, item is ${item}`,
-            item: JSON.parse(JSON.stringify(item))
-        });
-
-        delete urls[key];
     });
 
 }
