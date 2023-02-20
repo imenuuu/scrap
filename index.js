480356ce7b740951146851c46d4823f30a0f0cfb
mdwgti26 <mdwgti16@epopcon.com>
2023-02-20 14:44:23 +0900

    [index.js] - 코드 정리 - return value 수정

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ const chromeConfig = require('./src/config/chrome/ChromeConfig').options;
 const service = require('./src/config/service.json');
 const validator = require('./src/util/ValidatorUtil');
 const {del} = require("express/lib/application");
+const ColtItem = require("./src/dto/ColtItem");
 
+const API_PATH = '/acq/node/detail';
+const MAX_CONNECTIONS = 1;
+const MAX_IDLE_CONNECTIONS = 10;
 const urls = {};
 const priorities = {};
-const queueLength = 1;
 
 let cnt = 0;
 
@@ -28,74 +31,6 @@ function getPrimaryPriorityKey() {
     })[0];
 }
 
-async function launch() {
-    await appSetting(app);
-    // let collectSite = 'top.naverstore.com'
-    // let url = 'https://smartstore.naver.com/americanapparel/products/2536670070'
-    // let CLASS_PATH = validator.validateClassPath(service.detail, collectSite)
-    // let task = new detailTask(collectSite, CLASS_PATH, chromeConfig);
-    // let item = await task.execute(url);
-    // logger.info(`item: ${item}`)
-
-    app.post('/acq/node/detail', async (req, res) => {
-        try {
-            const collectSite = req.body.collectSite;
-            const url = req.body.url;
-            let key = `${collectSite}==${url}`;
-            priorities[key] = new Date();
-
-            while (true) {
-                if (Object.keys(urls).length < queueLength && key === getPrimaryPriorityKey()) {
-                    break;
-                }
-
-                if (Object.keys(priorities).length > 10) {
-                    delete priorities[key];
-
-                    let message = `CollectSite ${collectSite}, too many request, ${Object.keys(priorities).length}`;
-                    res.send({
-                        result: message,
-                        item: null
-                    });
-
-                    logger.info(message);
-                    return;
-                }
-
-                await sleep(1000);
-                logger.info(`queue is full, length: ${Object.keys(urls).length}, ${Object.keys(priorities).length}`);
-            }
-            delete priorities[key];
-            urls[key] = key;
-            logger.info(`${key}, ${Object.keys(urls).length}, ${Object.keys(priorities).length}`);
-
-
-            let item = null;
-            try {
-                let CLASS_PATH = validator.validateClassPath(service.detail, collectSite);
-                let task = new detailTask(collectSite, CLASS_PATH, chromeConfig);
-                item = await task.execute(url, cnt++);
-                cnt  = cnt > 300 ? 0 : cnt;
-            } catch (e) {
-                logger.error("detailTask error", e);
-            }
-
-            logger.info(`item: ${item}`);
-            // res.send(`CollectSite ${collectSite}, url is ${url}, item is ${item}`)
-            res.send({
-                result: `CollectSite ${collectSite}, url is ${url}, item is ${item}`,
-                item: JSON.parse(JSON.stringify(item))
-            });
-
-            delete urls[key];
-        } catch (e) {
-            logger.error('post error', e);
-        }
-    });
-
-}
-
-
 const appSetting = async function (app) {
     app.listen(port, () => {
         logger.info(`app listening at http://localhost:${port}`);
@@ -105,5 +40,90 @@ const appSetting = async function (app) {
     app.use(bodyParser.urlencoded({extended: false}));
 };
 
-launch();
+async function waitQueue(key, collectSite, res) {
+    priorities[key] = new Date();
 
+    while (true) {
+        if (Object.keys(urls).length < MAX_CONNECTIONS && key === getPrimaryPriorityKey()) {
+            break;
+        }
+
+        if (Object.keys(priorities).length > MAX_IDLE_CONNECTIONS) {
+            delete priorities[key];
+
+            let message = `CollectSite ${collectSite}, too many request, ${Object.keys(priorities).length}`;
+            res.send({
+                result: message,
+                item: null
+            });
+
+            logger.info(message);
+            return false;
+        }
+
+        await sleep(1000);
+        logger.info(`queue is full, length: ${Object.keys(urls).length}, ${Object.keys(priorities).length}`);
+    }
+    delete priorities[key];
+    urls[key] = key;
+    logger.info(`${key}, ${Object.keys(urls).length}, ${Object.keys(priorities).length}`);
+
+    return true;
+}
+
+
+function getClassType(item) {
+    if (item instanceof ColtItem) {
+        return "ColtItem";
+    }
+
+    return null;
+}
+
+/**
+ * let collectSite = 'top.naverstore.com'
+ * let url = 'https://smartstore.naver.com/americanapparel/products/2536670070'
+ * let CLASS_PATH = validator.validateClassPath(service.detail, collectSite)
+ * let task = new detailTask(collectSite, CLASS_PATH, chromeConfig);
+ * let item = await task.execute(url);
+ * logger.info(`item: ${item}`)
+ *
+ * return {
+ *     "type": "ColtItem", // dto class name
+ *     "item": item // item object
+ * }
+ */
+(async () => {
+    await appSetting(app);
+
+    app.post(API_PATH, async (req, res) => {
+        try {
+            const collectSite = req.body.collectSite;
+            const url = req.body.url;
+            let key = `${collectSite}==${url}`;
+            if (!await waitQueue(key, collectSite, res)) {
+                return;
+            }
+
+            let item = null;
+            try {
+                let CLASS_PATH = validator.validateClassPath(service.detail, collectSite);
+                let task = new detailTask(collectSite, CLASS_PATH, chromeConfig);
+                item = await task.execute(url, cnt++);
+                cnt = cnt > 300 ? 0 : cnt;
+            } catch (e) {
+                logger.error("detailTask error", e);
+            }
+
+            logger.info(`item: ${item}`);
+            res.send({
+                type: getClassType(item),
+                item: JSON.parse(JSON.stringify(item))
+            });
+
+            delete urls[key];
+        } catch (e) {
+            logger.error('post error', e);
+        }
+    });
+})()
