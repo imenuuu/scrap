86f3e11fb2a4fbfa02df647b852dfcc15a295c79
mdwgti26 <mdwgti16@epopcon.com>
2023-02-22 14:41:26 +0900

    - DetailTask export 수정 및 코드 정리

--- src/task/DetailTask.ts
+++ src/task/DetailTask.ts
@@ -1,25 +1,19 @@
-const logger = require('../config/logger/Logger')
-
-const puppeteer = require('puppeteer');
-const service = require('../config/service.json');
-class DetailTask{
+export class DetailTask {
     private _collectSite: any;
     private _dirName: any;
     private _config: any;
-    constructor(collectSite, dirName, chromeConfig){
+
+    constructor(collectSite, dirName, chromeConfig) {
         this._collectSite = collectSite;
         this._dirName = dirName;
         this._config = chromeConfig;
     }
 
-    async execute(url, cnt){
+    async execute(url, cnt) {
 
-        let detailClass = require(`${this._dirName}`)
-        let detail = new detailClass(this._config, this._collectSite, cnt);
+        const detailClass = require(`${this._dirName}`)
+        const detail = new detailClass(this._config, this._collectSite, cnt);
         const item = await detail.extractFromItemList(url);
         return item;
     }
-
 }
-
-module.exports = DetailTask;
