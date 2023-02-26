245583b8cf5a0d237014dc09f2eae35465567936
leeuigang <leeuigang@epopcon.com>
2023-02-26 11:52:20 +0900

    - chromeConfig proxyConfig추가

--- src/config/chrome/ChromeConfig.ts
+++ src/config/chrome/ChromeConfig.ts
@@ -15,19 +15,32 @@ var args = [
     ,'--disable-blink-features=AutomationControlled'
     ,'--window-size=1920,1080'
 ]
+const proxyConfig = {
+    proxyName : 'luminati', //oxyLab, luminati 기본 셋팅은 oxyLab
+    /*username : 'epopcon',//
+    password : 'FChB5uEd45', oxyLab
+    */
+    username: 'lum-customer-epopcon-zone-zone_ru',
+    password: 'jhwfsy8ucuh2',
+    key  : '4b33bfee-80a6-11eb-927e-901b0ec4424b', //oxylab
+    contextUse : true
+}
 
 export const options = {
     args,
-    headless: true,
+    proxyConfig,
+    headless: false,
     ignoreHTTPSErrors: true,
     defaultViewport: null,
     // executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-    //executablePath: "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe", //for window
+    executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe", //for window
     //executablePath: "/usr/bin/google-chrome-stable", //for deploy
 
     // userDataDir: '/Users/usr/git/wspider_node/puppet_profile/detail'
     // product: "chrome"
+
 };
+
 
 export async function navigatorSet (page) {
     await page.evaluateOnNewDocument(() => {
